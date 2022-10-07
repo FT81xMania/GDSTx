0d8611a637834e9396e03a901c32119b1551f41a
FT81x Manía <ft81xmania@gmail.com>
2022-10-07 09:50:42 -0500

    Add files via upload

--- examples/1.Basic/Helloworld_border/Helloworld_border.ino
+++ examples/1.Basic/Helloworld_border/Helloworld_border.ino
@@ -1,1 +1,27 @@
+#include <GDSTx.h>
 
+int MCUID;
+
+void setup()
+{
+  GD.begin();
+  IDEMCU();
+}
+
+void loop()
+{
+GD.ClearColorRGB(0x100020);
+GD.Clear();
+  GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world");
+
+GD.Begin(LINES);
+  GD.ColorRGB(255,255,255);
+  GD.Vertex2f(0*16, 0*16);    GD.Vertex2f((GD.w)*16, 0*16);  //Superior
+  GD.Vertex2f(0*16, (GD.h-1)*16);  GD.Vertex2f((GD.w)*16, (GD.h-1)*16); //inferior
+  GD.Vertex2f(0*16, 0*16);  GD.Vertex2f(0*16, (GD.h-1)*16); //izquierda
+  GD.Vertex2f((GD.w-1)*16, 0*16);  GD.Vertex2f((GD.w-1)*16, (GD.h-1)*16); //derecha  
+  
+Parametros();
+  
+GD.swap();
+} 
