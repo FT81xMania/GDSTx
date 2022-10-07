73032ed49c1b1a3fc58d6298ec83750577551364
FT81x Manía <ft81xmania@gmail.com>
2022-10-07 10:38:22 -0500

    Add files via upload

--- examples/1.Basic/Angle_txt/Angle_txt.ino
+++ examples/1.Basic/Angle_txt/Angle_txt.ino
@@ -1,4 +1,7 @@
 #include <GDSTx.h>
+                     //       0       1
+char MCUTXT[60][22]={"F411CE", "Teesy 4.1"};
+int MCUbase=1;
 
 Bitmap Background,SaidHello;
 
@@ -6,8 +9,7 @@ void setup()
 {
   GD.begin();
   Background.fromfile("dog.jpg");
-  SaidHello.fromtext(28, "Teensy 4@FT8XX");
-  //SaidHello.fromtext(28, "STM32@FT8XX");
+  SaidHello.fromtext(20, MCUTXT[MCUbase]);
   MP();
 }
 void loop(){}
@@ -15,35 +17,37 @@ void loop(){}
 void MP()
 {
   int ANG=0;
-  int minX = 0, minY = 0;
-  int maxX = GD.w - minX, maxY = GD.h - minY;
-  int startX = maxX / 2, startY = maxY / 2;
-  int deltaX = 4, deltaY = 4;
+
+  int minPX = 15; int maxPX = GD.w-minPX;
+  int movX = minPX;
+  int deltaenX = 1;
+
+  int minPY = 15; int maxPY = GD.h-minPY;
+  int movY = minPY;
+  int deltaenY = 1;  
 
 while(1)
 {
   ANG = ANG + 1;
   if (ANG>=360){ANG=0;}
 
-   startX = startX + deltaX;   startY = startY + deltaY;
-   if (startX >= maxX)
-       deltaX = -deltaX;
-   if (startX <= minX)
-       deltaX = abs(deltaX);
-   if (startY >= maxY)
-       deltaY = -deltaY;
-   if (startY <= minY)
-       deltaY = abs(deltaY);
+  movX = movX + deltaenX;
+  if(movX >= maxPX){deltaenX = -1;}
+  if(movX <= minPX){deltaenX = 1;}
+
+  movY = movY + deltaenY;
+  if(movY >= maxPY){deltaenY = -1;}
+  if(movY <= minPY){deltaenY = 1;}  
   
   GD.ClearColorRGB(0x000055);
   GD.Clear();
-
+  GD.SaveContext();
   GD.ColorA(144);
   Background.draw(GD.w / 2, GD.h / 2);
-  GD.ColorA(255);
-  
-  GD.ColorRGB(0xffffff); SaidHello.draw(startX, startY, DEGREES(ANG));
-
+   GD.ColorA(255);
+   GD.ColorRGB(0xffffff); SaidHello.draw(10, 180, DEGREES(270));
+   GD.ColorRGB(0xffffff); SaidHello.draw(movX, movY, DEGREES(ANG*(-1)));
+  GD.RestoreContext();
   GD.swap(); 
  }
 }
