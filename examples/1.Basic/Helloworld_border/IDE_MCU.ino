2e224abfe97d212fa23ce365baaf0a11d76752ed
FT81x Manía <ft81xmania@gmail.com>
2025-01-10 15:49:15 -0600

    Add files via upload

--- examples/1.Basic/Helloworld_border/IDE_MCU.ino
+++ examples/1.Basic/Helloworld_border/IDE_MCU.ino
@@ -59,8 +59,18 @@ void IDEMCU()
      sprintf(Board, "MCU: ESP32");
   #endif
 
-  #if !defined(TEENSYDUINO)&& !defined(ESP32)&& !defined(__DUE__)&& !defined(ESP8266)&& !defined(ARDUINO_ARCH_STM32)&& defined(ARDUINO)
+  #if defined(ARDUINO_ARCH_RP2040)
      ClaveBoard=6;
+     sprintf(Board, "MCU: R-Pi PICO");
+  #endif
+
+  #if defined(ARDUINO_UNOR4_MINIMA)
+     ClaveBoard=7;
+     sprintf(Board, "MCU: UNO R4 MINIMA");
+  #endif
+
+  #if !defined(TEENSYDUINO)&& !defined(ESP32)&& !defined(__DUE__)&& !defined(ESP8266)&& !defined(ARDUINO_ARCH_STM32)&& !defined(ARDUINO_UNOR4_MINIMA)&& !defined(ARDUINO_ARCH_RP2040)&& defined(ARDUINO)
+     ClaveBoard=8;
      sprintf(Board, "MCU: Arduino MEGA/UNO");
   #endif
 }
