2a3695249f3e08097a20f5440e686f2d39c132d1
FT81x Manía <ft81xmania@gmail.com>
2025-01-17 09:17:22 -0600

    Add files via upload

--- examples/2.Advanced/jpeg_bench/Parametros.ino
+++ examples/2.Advanced/jpeg_bench/Parametros.ino
@@ -16,13 +16,13 @@ void Parametros()
   }
   #endif
   
-  if(SizeFT813==35){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-3.5-320240FT-CSXN-CTP");}
-  if(SizeFT813==43){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-4.3-480272FT-CSXN-CTP");}
-  if(SizeFT813==5){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-5.0-800480FT-CTXL-CTP");}
-  if(SizeFT813==7){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-7.0-800480FT-CSXN-CTP");}
-  if(SizeFT813==51){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@Riverdi RVT50UQFNWC000");}
-  if(SizeFT813==71){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@Riverdi RVT70UQFNWC000");}
-  if(SizeFT813==52){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: BT815@Matrix Orbital EVE3-50G");}
+  if(SizeEVE==35){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-3.5-320240FT-CSXN-CTP");}
+  if(SizeEVE==43){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-4.3-480272FT-CSXN-CTP");}
+  if(SizeEVE==5){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-5.0-800480FT-CTXL-CTP");}
+  if(SizeEVE==7){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@NHD-7.0-800480FT-CSXN-CTP");}
+  if(SizeEVE==51){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@Riverdi RVT50UQFNWC000");}
+  if(SizeEVE==71){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: FT813@Riverdi RVT70UQFNWC000");}
+  if(SizeEVE==52){GD.cmd_text(2, GD.h-12, 20, 0, "EVE chip: BT815@Matrix Orbital EVE3-50G");}
   
   
   GD.RestoreContext();
